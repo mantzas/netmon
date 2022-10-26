089831ae3be0b29056714c3ea0017e14ee425ab2
Sotirios Mantziaris <smantziaris@gmail.com>
2022-10-26 22:13:08 +0300

    Linting fixes

--- speed/speed.go
+++ speed/speed.go
@@ -69,12 +69,27 @@ func (sm *Monitor) Monitor(ctx context.Context) {
 
 func (sm *Monitor) measure(ctx context.Context, srv *speedtest.Server) {
 	serverName := fmt.Sprintf("%s - %s", srv.ID, srv.Sponsor)
-	srv.PingTestContext(ctx)
-	srv.DownloadTestContext(ctx, false)
-	srv.UploadTestContext(ctx, false)
+
+	err := srv.PingTestContext(ctx)
+	if err != nil {
+		sm.logger.Printf("speedtest: failed pint test: %v\n", err)
+		return
+	}
+
+	err = srv.DownloadTestContext(ctx, false)
+	if err != nil {
+		sm.logger.Printf("speedtest: failed download test: %v\n", err)
+		return
+	}
+
+	err = srv.UploadTestContext(ctx, false)
+	if err != nil {
+		sm.logger.Printf("speedtest: failed upload test: %v\n", err)
+		return
+	}
 
 	sm.logger.Printf("speedtest for host: %s, latency: %s, dl: %f, ul: %f\n", serverName, srv.Latency, srv.DLSpeed, srv.ULSpeed)
-	err := sm.metricAPI.ReportSpeed(ctx, srv)
+	err = sm.metricAPI.ReportSpeed(ctx, srv)
 	if err != nil {
 		sm.logger.Printf("speedtest: failed report metrics: %v\n", err)
 		return
