1fb210501ab0e7223e3de415ef9e7e72e812c4e6
Sotirios Mantziaris <smantziaris@gmail.com>
2023-12-30 21:36:00 +0200

    Introduce OpenTelemetry (#44)
    Resolves #41

--- cmd/main.go
+++ cmd/main.go
@@ -17,6 +17,26 @@ import (
 
 	"github.com/mantzas/netmon"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
+	"go.opentelemetry.io/otel/propagation"
+	"go.opentelemetry.io/otel/sdk/resource"
+	"go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.21.0"
+)
+
+const (
+	httpPortName                 = "NETMON_HTTP_PORT"
+	httpPortDefaultValue         = "8092"
+	otlpGRPCEndpointName         = "NETMON_OTLP_GRPC_ENDPOINT"
+	otlpGRPCEndpointDefaultValue = "localhost:4317"
+	speedServerIDs               = "NETMON_SPEED_SERVER_IDS"
+)
+
+var (
+	serviceName    = "netmon"
+	serviceVersion = "0.1.0"
 )
 
 func main() {
@@ -37,46 +57,73 @@ func run() error {
 		return err
 	}
 
+	gRPCEndpoint, err := getGRPCEndpoint()
+	if err != nil {
+		return err
+	}
+
 	slog.Info("start monitoring", "port", port, "servers", servers)
 
-	chSignal := make(chan os.Signal, 1)
-	signal.Notify(chSignal, os.Interrupt, syscall.SIGTERM)
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
+	otelShutdown, err := setupOTelSDK(ctx, serviceName, serviceVersion, gRPCEndpoint)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		err = errors.Join(err, otelShutdown(context.Background()))
+	}()
 
 	srv := createHTTPServer(port, servers)
 
+	srvErr := make(chan error, 1)
+
 	go func() {
-		err = srv.ListenAndServe()
-		if !errors.Is(err, http.ErrServerClosed) {
-			slog.Error("failed to run HTTP listener", "err", err)
-			os.Exit(1)
-		}
+		srvErr <- srv.ListenAndServe()
 	}()
 
-	sig := <-chSignal
-	slog.Info("signal received", "sig", sig)
+	select {
+	case err = <-srvErr:
+		return err
+	case <-ctx.Done():
+		// Wait for first CTRL+C.
+		// Stop receiving signal notifications as soon as possible.
+		slog.Info("interrupts received, shutting down")
+		stop()
+	}
 
 	ctx, cnl := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cnl()
 
 	err = srv.Shutdown(ctx)
 	if err != nil {
-		slog.Info("failed to shutdown server", "err", err)
+		return fmt.Errorf("failed to shutdown server: %w", err)
 	}
 
-	return err
+	slog.Info("server shutdown completed")
+	return nil
 }
 
 func createHTTPServer(port int, servers []string) *http.Server {
 	mux := http.NewServeMux()
+	handleFunc := func(pattern string, hd func(http.ResponseWriter, *http.Request)) {
+		handler := otelhttp.WithRouteTag(pattern, http.HandlerFunc(hd))
+		mux.Handle(pattern, handler)
+	}
+
 	mux.Handle("/metrics", promhttp.Handler())
-	mux.HandleFunc("/health", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("/health", http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
 		w.WriteHeader(http.StatusOK)
 	}))
-	mux.HandleFunc("/ready", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("/ready", http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
 		w.WriteHeader(http.StatusOK)
 	}))
-	mux.HandleFunc("/api/v1/ping", pingHandler)
-	mux.HandleFunc("/api/v1/speed", speedHandlerFunc(servers))
+
+	handleFunc("/api/v1/ping", pingHandler)
+	handleFunc("/api/v1/speed", speedHandlerFunc(servers))
+	// Add HTTP instrumentation for the whole server.
+	otelHandler := otelhttp.NewHandler(mux, "/")
 
 	return &http.Server{
 		Addr:              fmt.Sprintf(":%d", port),
@@ -84,7 +131,7 @@ func createHTTPServer(port int, servers []string) *http.Server {
 		ReadHeaderTimeout: 10 * time.Second,
 		WriteTimeout:      60 * time.Second,
 		IdleTimeout:       120 * time.Second,
-		Handler:           http.TimeoutHandler(mux, 59*time.Second, ""),
+		Handler:           http.TimeoutHandler(otelHandler, 59*time.Second, ""),
 	}
 }
 
@@ -144,7 +191,7 @@ func speedHandlerFunc(serverIds []string) http.HandlerFunc {
 }
 
 func getPort() (int, error) {
-	port, err := getEnv("HTTP_PORT", "8092")
+	port, err := getEnv(httpPortName, httpPortDefaultValue)
 	if err != nil {
 		return 0, err
 	}
@@ -157,8 +204,12 @@ func getPort() (int, error) {
 	return portInt, nil
 }
 
+func getGRPCEndpoint() (string, error) {
+	return getEnv(otlpGRPCEndpointName, otlpGRPCEndpointDefaultValue)
+}
+
 func getServerIDs() ([]string, error) {
-	ids, err := getEnv("SPEED_SERVER_IDS", "")
+	ids, err := getEnv(speedServerIDs, "")
 	if err != nil {
 		return nil, err
 	}
@@ -187,4 +238,82 @@ func getEnv(key string, def string) (string, error) {
 	}
 
 	return "", fmt.Errorf("env var %s does not exist and no default value is set", key)
+}
+
+func setupOTelSDK(ctx context.Context, serviceName, serviceVersion, gRPCEndpoint string) (shutdown func(context.Context) error, err error) {
+	var shutdownFuncs []func(context.Context) error
+
+	// shutdown calls cleanup functions registered via shutdownFuncs.
+	// The errors from the calls are joined.
+	// Each registered cleanup will be invoked once.
+	shutdown = func(ctx context.Context) error {
+		var err error
+		for _, fn := range shutdownFuncs {
+			err = errors.Join(err, fn(ctx))
+		}
+		shutdownFuncs = nil
+		return err
+	}
+
+	// handleErr calls shutdown for cleanup and makes sure that all errors are returned.
+	handleErr := func(inErr error) {
+		err = errors.Join(inErr, shutdown(ctx))
+	}
+
+	// Set up resource.
+	res, err := newResource(serviceName, serviceVersion)
+	if err != nil {
+		handleErr(err)
+		return
+	}
+
+	// Set up propagator.
+	prop := newPropagator()
+	otel.SetTextMapPropagator(prop)
+
+	// Set up trace provider.
+	tracerProvider, err := newTraceProvider(ctx, gRPCEndpoint, res)
+	if err != nil {
+		handleErr(err)
+		return
+	}
+	shutdownFuncs = append(shutdownFuncs, tracerProvider.Shutdown)
+	otel.SetTracerProvider(tracerProvider)
+
+	return
+}
+
+func newResource(serviceName, serviceVersion string) (*resource.Resource, error) {
+	return resource.Merge(resource.Default(),
+		resource.NewWithAttributes(semconv.SchemaURL,
+			semconv.ServiceName(serviceName),
+			semconv.ServiceVersion(serviceVersion),
+		))
+}
+
+func newPropagator() propagation.TextMapPropagator {
+	return propagation.NewCompositeTextMapPropagator(
+		propagation.TraceContext{},
+		propagation.Baggage{},
+	)
+}
+
+func newTraceProvider(ctx context.Context, endpoint string, res *resource.Resource) (*trace.TracerProvider, error) {
+	options := []otlptracegrpc.Option{
+		otlptracegrpc.WithEndpoint(endpoint),
+		otlptracegrpc.WithInsecure(),
+		otlptracegrpc.WithTimeout(5 * time.Second),
+	}
+
+	traceExporter, err := otlptracegrpc.New(ctx, options...)
+	if err != nil {
+		return nil, err
+	}
+
+	traceProvider := trace.NewTracerProvider(
+		trace.WithBatcher(traceExporter, trace.WithBatchTimeout(5*time.Second)),
+		trace.WithResource(res),
+		trace.WithSampler(trace.AlwaysSample()),
+	)
+	return traceProvider, nil
 }
