00b3c2b58c0edd75a6158ea105f88f6d77dc626c
Sotirios Mantziaris <smantziaris@gmail.com>
2024-08-15 20:06:42 +0300

    CLI to ping and speed test (#121)
    The code changes modify the requests made to the API endpoints
    `/api/v1/ping` and `/api/v1/speed` by appending a server ID parameter.
    This allows the application to retrieve speed data specific to a
    particular server.
    
    Recent user commits are not provided.
    
    Resolves #120 .

---
+++ cmd/cli/main.go
@@ -0,0 +1,142 @@
+// Package main is the entrypoint of the application.
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"flag"
+	"fmt"
+	"log/slog"
+	"os"
+	"strings"
+
+	"github.com/mantzas/netmon"
+	"github.com/mantzas/netmon/otelsdk"
+	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+)
+
+const (
+	serviceName = "netmon-cli"
+	apiV1Prefix = "/api/v1/"
+)
+
+var (
+	serviceVersion      = "0.1.0"
+	serverIDsEnvName    = "NETMON_SPEED_SERVER_IDS"
+	serverURLEnvVarName = "NETMON_SERVER_URL"
+)
+
+func main() {
+	args, err := parseArguments()
+	if err != nil {
+		slog.Error("failed to parse flags", "err", err)
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
+	ctx := context.Background()
+
+	otelShutdown, err := otelsdk.Setup(ctx, serviceName, serviceVersion)
+	if err != nil {
+		slog.Error("failed to setup otel", "err", err)
+		os.Exit(1)
+	}
+
+	err = executeRequest(ctx, args)
+	err = errors.Join(err, otelShutdown(context.Background()))
+	if err == nil {
+		return
+	}
+
+	slog.Error("failed to execute request", "err", err)
+	os.Exit(1)
+}
+
+type argument struct {
+	cmd       string
+	serverURL string
+	serverIDs []string
+}
+
+func parseArguments() (argument, error) {
+	var cmd string
+	var serverIDsValue string
+	var serverURL string
+	flag.StringVar(&cmd, "cmd", "ping", "Can be either ping or speed.")
+	flag.StringVar(&serverIDsValue, "servers", "5188", "A comma separated list of server IDs.")
+	flag.StringVar(&serverURL, "url", "http://localhost:8092", "The URL of the netmon service.")
+	flag.Parse()
+
+	if cmd != "ping" && cmd != "speed" {
+		return argument{}, fmt.Errorf("unknown cmd flag value: %s", cmd)
+	}
+
+	if url, ok := os.LookupEnv(serverURLEnvVarName); ok {
+		serverURL = url
+	}
+
+	if ids, ok := os.LookupEnv(serverIDsEnvName); ok {
+		serverIDsValue = ids
+	}
+
+	return argument{
+		cmd:       cmd,
+		serverIDs: strings.Split(serverIDsValue, ","),
+		serverURL: serverURL,
+	}, nil
+}
+
+func executeRequest(ctx context.Context, args argument) error {
+	ctx, span := otel.Tracer(serviceName).Start(ctx, args.cmd)
+	defer span.End()
+	span.SetAttributes(attribute.String("cmd", args.cmd))
+	span.SetAttributes(attribute.String("server_ids", strings.Join(args.serverIDs, ",")))
+
+	targetURL := args.serverURL + apiV1Prefix + args.cmd + "/" + strings.Join(args.serverIDs, ",")
+
+	resp, err := otelhttp.Get(ctx, targetURL)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		err := resp.Body.Close()
+		if err != nil {
+			slog.Error("failed to close response body", "cmd", args.cmd, "err", err)
+		}
+	}()
+
+	if resp.StatusCode != 200 {
+		return fmt.Errorf("unexpected status code: %d for %s request", resp.StatusCode, args.cmd)
+	}
+
+	var resultsAttr slog.Attr
+
+	switch args.cmd {
+	case "ping":
+		c := struct {
+			Results []netmon.PingResult `json:"results"`
+		}{}
+		err = json.NewDecoder(resp.Body).Decode(&c)
+		if err != nil {
+			return fmt.Errorf("failed to decode ping response: %w", err)
+		}
+
+		resultsAttr = slog.Int("results", len(c.Results))
+
+	case "speed":
+		c := struct {
+			Results []netmon.SpeedResult `json:"results"`
+		}{}
+		err = json.NewDecoder(resp.Body).Decode(&c)
+		if err != nil {
+			return fmt.Errorf("failed to decode ping response: %w", err)
+		}
+		resultsAttr = slog.Int("results", len(c.Results))
+	}
+
+	slog.InfoContext(ctx, "request executed successfully", slog.String("cmd", args.cmd), resultsAttr)
+	return nil
+}
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -20,22 +20,14 @@ import (
 	_ "github.com/grafana/pyroscope-go/godeltaprof/http/pprof"
 
 	"github.com/mantzas/netmon"
+	"github.com/mantzas/netmon/otelsdk"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
-	"go.opentelemetry.io/otel"
-	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
-	"go.opentelemetry.io/otel/propagation"
-	"go.opentelemetry.io/otel/sdk/resource"
-	"go.opentelemetry.io/otel/sdk/trace"
-	semconv "go.opentelemetry.io/otel/semconv/v1.26.0"
 )
 
 const (
-	httpPortName                 = "NETMON_HTTP_PORT"
-	httpPortDefaultValue         = "8092"
-	otlpGRPCEndpointName         = "NETMON_OTLP_GRPC_ENDPOINT"
-	otlpGRPCEndpointDefaultValue = "localhost:4317"
-	speedServerIDs               = "NETMON_SPEED_SERVER_IDS"
+	httpPortName         = "NETMON_HTTP_PORT"
+	httpPortDefaultValue = "8092"
 )
 
 const (
@@ -52,27 +44,17 @@ func main() {
 }
 
 func run() error {
-	servers, err := getServerIDs()
-	if err != nil {
-		return err
-	}
-
 	port, err := getPort()
 	if err != nil {
 		return err
 	}
 
-	gRPCEndpoint, err := getGRPCEndpoint()
-	if err != nil {
-		return err
-	}
-
-	slog.Info("start monitoring", "port", port, "servers", servers)
+	slog.Info("start monitoring", "port", port)
 
 	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
 	defer stop()
 
-	otelShutdown, err := setupOTelSDK(ctx, serviceName, serviceVersion, gRPCEndpoint)
+	otelShutdown, err := otelsdk.Setup(ctx, serviceName, serviceVersion)
 	if err != nil {
 		return err
 	}
@@ -80,7 +62,7 @@ func run() error {
 		err = errors.Join(err, otelShutdown(context.Background()))
 	}()
 
-	srv := createHTTPServer(port, servers)
+	srv := createHTTPServer(port)
 
 	srvErr := make(chan error, 1)
 
@@ -110,7 +92,7 @@ func run() error {
 	return nil
 }
 
-func createHTTPServer(port int, servers []string) *http.Server {
+func createHTTPServer(port int) *http.Server {
 	mux := http.NewServeMux()
 	handleFunc := func(pattern string, hd func(http.ResponseWriter, *http.Request)) {
 		handler := otelhttp.WithRouteTag(pattern, http.HandlerFunc(hd))
@@ -119,17 +101,16 @@ func createHTTPServer(port int, servers []string) *http.Server {
 	}
 
 	mux.Handle("/metrics", promhttp.Handler())
-	mux.HandleFunc("/health", http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
-		w.WriteHeader(http.StatusOK)
-	}))
-	mux.HandleFunc("/ready", http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
-		w.WriteHeader(http.StatusOK)
-	}))
-
-	handleFunc("/api/v1/ping", pingHandlerFunc(servers))
-	handleFunc("/api/v1/speed", speedHandlerFunc(servers))
-
 	mux.Handle("/debug/pprof/", http.DefaultServeMux)
+	mux.HandleFunc("GET /health", http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	}))
+	mux.HandleFunc("GET /ready", http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	}))
+
+	handleFunc("GET /api/v1/ping/{ids}", pingHandlerFunc())
+	handleFunc("GET /api/v1/speed/{ids}", speedHandlerFunc())
 
 	return &http.Server{
 		Addr:              fmt.Sprintf(":%d", port),
@@ -145,8 +126,26 @@ type pingResponse struct {
 	Results []netmon.PingResult `json:"results"`
 }
 
-func pingHandlerFunc(serverIDs []string) http.HandlerFunc {
+func getServerIDs(r *http.Request) ([]string, error) {
+	idsString := r.PathValue("ids")
+	if idsString == "" {
+		return nil, fmt.Errorf("ping failed. missing server ids value")
+	}
+
+	return strings.Split(idsString, ","), nil
+}
+
+func pingHandlerFunc() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		serverIDs, err := getServerIDs(r)
+		if err != nil {
+			slog.ErrorContext(r.Context(), "missing server ids in ping request", "err", err)
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		slog.InfoContext(r.Context(), "ping request", "server_ids", serverIDs)
+
 		results, err := netmon.Ping(r.Context(), serverIDs)
 		if err != nil {
 			slog.ErrorContext(r.Context(), "ping failed", "err", err)
@@ -176,8 +175,17 @@ type speedResponse struct {
 	Results []netmon.SpeedResult `json:"results"`
 }
 
-func speedHandlerFunc(serverIDs []string) http.HandlerFunc {
+func speedHandlerFunc() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		serverIDs, err := getServerIDs(r)
+		if err != nil {
+			slog.ErrorContext(r.Context(), "missing server ids in speed request", "err", err)
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		slog.InfoContext(r.Context(), "speed request", "server_ids", serverIDs)
+
 		results := netmon.Speed(r.Context(), serverIDs)
 
 		response, err := json.Marshal(speedResponse{Results: results})
@@ -212,25 +220,6 @@ func getPort() (int, error) {
 	return portInt, nil
 }
 
-func getGRPCEndpoint() (string, error) {
-	return getEnv(otlpGRPCEndpointName, otlpGRPCEndpointDefaultValue)
-}
-
-func getServerIDs() ([]string, error) {
-	ids, err := getEnv(speedServerIDs, "")
-	if err != nil {
-		return nil, err
-	}
-
-	serverIDs := strings.Split(ids, ",")
-
-	if len(serverIDs) == 0 {
-		return nil, fmt.Errorf("no valid server ids provided in the env var")
-	}
-
-	return serverIDs, nil
-}
-
 func getEnv(key string, def string) (string, error) {
 	value, ok := os.LookupEnv(key)
 	if !ok && def == "" {
@@ -246,82 +235,4 @@ func getEnv(key string, def string) (string, error) {
 	}
 
 	return "", fmt.Errorf("env var %s does not exist and no default value is set", key)
-}
-
-func setupOTelSDK(ctx context.Context, serviceName, serviceVersion, gRPCEndpoint string) (shutdown func(context.Context) error, err error) {
-	var shutdownFuncs []func(context.Context) error
-
-	// shutdown calls cleanup functions registered via shutdownFuncs.
-	// The errors from the calls are joined.
-	// Each registered cleanup will be invoked once.
-	shutdown = func(ctx context.Context) error {
-		var err error
-		for _, fn := range shutdownFuncs {
-			err = errors.Join(err, fn(ctx))
-		}
-		shutdownFuncs = nil
-		return err
-	}
-
-	// handleErr calls shutdown for cleanup and makes sure that all errors are returned.
-	handleErr := func(inErr error) {
-		err = errors.Join(inErr, shutdown(ctx))
-	}
-
-	// Set up resource.
-	res, err := newResource(serviceName, serviceVersion)
-	if err != nil {
-		handleErr(err)
-		return
-	}
-
-	// Set up propagator.
-	prop := newPropagator()
-	otel.SetTextMapPropagator(prop)
-
-	// Set up trace provider.
-	tracerProvider, err := newTraceProvider(ctx, gRPCEndpoint, res)
-	if err != nil {
-		handleErr(err)
-		return
-	}
-	shutdownFuncs = append(shutdownFuncs, tracerProvider.Shutdown)
-	otel.SetTracerProvider(tracerProvider)
-
-	return
-}
-
-func newResource(serviceName, serviceVersion string) (*resource.Resource, error) {
-	return resource.Merge(resource.Default(),
-		resource.NewWithAttributes(semconv.SchemaURL,
-			semconv.ServiceName(serviceName),
-			semconv.ServiceVersion(serviceVersion),
-		))
-}
-
-func newPropagator() propagation.TextMapPropagator {
-	return propagation.NewCompositeTextMapPropagator(
-		propagation.TraceContext{},
-		propagation.Baggage{},
-	)
-}
-
-func newTraceProvider(ctx context.Context, endpoint string, res *resource.Resource) (*trace.TracerProvider, error) {
-	options := []otlptracegrpc.Option{
-		otlptracegrpc.WithEndpoint(endpoint),
-		otlptracegrpc.WithInsecure(),
-		otlptracegrpc.WithTimeout(5 * time.Second),
-	}
-
-	traceExporter, err := otlptracegrpc.New(ctx, options...)
-	if err != nil {
-		return nil, err
-	}
-
-	traceProvider := trace.NewTracerProvider(
-		trace.WithBatcher(traceExporter, trace.WithBatchTimeout(5*time.Second)),
-		trace.WithResource(res),
-		trace.WithSampler(trace.AlwaysSample()),
-	)
-	return traceProvider, nil
 }
